f0118b756a0618ba369c798d682af3719c48d4cd
unknown <illshaad.budureea@avec.fr>
2021-07-14 22:56:40 +0200

    moyenne des restaurants ok

--- src/Components/GoogleMap/Modal/Modal.jsx
+++ src/Components/GoogleMap/Modal/Modal.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { StarFilled } from "@ant-design/icons";
 import { Modal } from "antd";
 import "antd/dist/antd.css";
 
-export default function ModalComponsant({ text, open, onCancel, children }) {
+export default function ModalComponsant({ moyenne, open, onCancel, children }) {
   return (
     <Modal
-      title="Bienvenue"
-      onOk={() => console.log("test")}
+      title={
+        <>
+          Bienvenue la moyenne de ce restaurant est de :
+          <span>
+            {moyenne} <StarFilled />
+          </span>
+        </>
+      }
       onCancel={onCancel}
       visible={open}
       footer={[]}
--- src/Components/GoogleMap/Restaurant/Restaurant.jsx
+++ src/Components/GoogleMap/Restaurant/Restaurant.jsx
@@ -1,37 +1,38 @@
-import React, { useState } from "react";
+import React, { useLayoutEffect, useState } from "react";
 import TextArea from "../../../Components/GoogleMap/ui/form/Textarea";
 import { Card, Button, Rate, Form, Input } from "antd";
 import ModalComponsant from "../Modal/Modal";
 import styled from "styled-components";
 import smallKey from "../../../services/smallKeyGen";
 import "antd/dist/antd.css";
-import { useForm, Controller, set } from "react-hook-form";
+import { useForm } from "react-hook-form";
 
 export default function RestaurantComponent({ e }) {
-  console.log(e);
+  // console.log(e.ratings);
+
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [isModalVisibleTwo, setIsModalVisibleTwo] = useState(false);
-  const [valueRating, setValueRating] = useState();
+  const [currentValue, setCurrentValue] = useState();
 
-  const { register, handleSubmit, watch } = useForm();
+  const { register, handleSubmit } = useForm();
 
-  const Text = watch("message");
-  console.log(Text);
+  // const message = watch("message");
+  // console.log(message);
 
   const submit = (data) => {
+    // console.log(data, "  dataaaa");
+
     setIsModalVisibleTwo(!true);
     setIsModalVisible(!false);
   };
 
-  const somme = e[0].map((f) => {
-    return f.ratings[0].stars;
-  });
-
-  console.log(somme);
-
-  const reducer = (a, b) => (a + b) / e.ratings.length;
-  const final = somme.reduce(reducer);
-  console.log(final, " MOYENNE");
+  const ratings = e.ratings;
+  const total = [];
+  for (let i = 0; i < ratings.length; i++) {
+    total.push(ratings[i].stars);
+  }
+  const reducer = (a, b) => (a + b) / total.length;
+  const moyenne = total.reduce(reducer);
 
   return (
     <Card title={e.restaurantName} style={{ width: 300 }}>
@@ -43,6 +44,7 @@ export default function RestaurantComponent({ e }) {
         <ModalComponsant
           open={isModalVisible}
           onCancel={() => setIsModalVisible(false)}
+          moyenne={moyenne}
         >
           {e.ratings.map((e) => (
             <div key={smallKey(10)}>
@@ -59,6 +61,7 @@ export default function RestaurantComponent({ e }) {
           Ajouter un avis
         </Button>
         <ModalComponsant
+          moyenne={moyenne}
           open={isModalVisibleTwo}
           onCancel={() => setIsModalVisibleTwo(false)}
         >
@@ -66,7 +69,12 @@ export default function RestaurantComponent({ e }) {
           <form onSubmit={handleSubmit(submit)}>
             <TextArea {...register("message")}></TextArea>
             <P>Notez moi !</P>
-            <Rate />
+            <Rate
+              onChange={(value) => {
+                setCurrentValue(value);
+              }}
+              value={currentValue}
+            />
             <button type="submit">cliquer</button>
           </form>
         </ModalComponsant>
